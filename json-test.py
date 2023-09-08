1e661078f74fe80d4c09157f0093c5bcfc4da7c7
leyi <leyi@myun.info>
2023-09-08 14:23:52 +0800

    prompt

---
+++ json-test.py
@@ -0,0 +1,14 @@
+'''
+Author: leyi leyi@myun.info
+Date: 2023-08-23 14:33:14
+LastEditors: leyi leyi@myun.info
+LastEditTime: 2023-08-23 17:44:55
+FilePath: /learning-openai/json-test.py
+Description: 
+
+Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
+'''
+
+cats = [1, 2, 3]
+cats_1 = [1, 2, 3]
+print(cats == cats_1)
